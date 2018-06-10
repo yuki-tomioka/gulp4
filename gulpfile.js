1b8884d26846ae01b0d80101ea56d2252df2216a
yuki tomioka <yuli_tomioka@zeroxeed.co.jp>
2018-06-10 21:02:01 +0900

    追加：初期設定

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+/*===================================================
+          読み込み
+===================================================*/
+const gulp = require('gulp')
+
+// sass関連読み込み
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const flexBugsFixes = require('postcss-flexbugs-fixes')
+const cssWring = require('csswring')
+const sourcemaps = require('gulp-sourcemaps')
+const cssMqpacker = require('css-mqpacker')
+const concat = require('gulp-concat')
+
+// 画像圧縮関連読み込み
+const imagemin = require('gulp-imagemin')
+const imageminPngquant = require('imagemin-pngquant')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+
+// ブラウザ関連読み込み
+const browserSync = require('browser-sync').create()
+
+/*===================================================
+          設定
+===================================================*/
+// sass関連設定
+const autoprefixerOption = {
+  grid: true
+}
+
+const postcssOption = [
+  flexBugsFixes,
+  autoprefixer(autoprefixerOption),
+  cssMqpacker,
+  cssWring
+]
+
+// 画像圧縮関連設定
+const imageminOption = [
+  imageminPngquant({ quality: '65-80' }),
+  imageminMozjpeg({ quality: '80' }),
+  imagemin.gifsicle(),
+  imagemin.jpegtran(),
+  imagemin.optipng(),
+  imagemin.svgo()
+]
+
+// ブラウザ関連設定
+const browserSyncOption = {
+  server: './dist'
+}
+/*===================================================
+          タスク定義
+===================================================*/
+// sassコンパイル
+gulp.task('sass', () => {
+  return gulp
+    .src('./src/assets/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss(postcssOption))
+    .pipe(concat('style.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/assets/css/'))
+})
+
+// 画像圧縮
+gulp.task('imagemin', () => {
+  return gulp
+    .src('./src/**/images/*')
+    .pipe(imagemin(imageminOption))
+    .pipe(gulp.dest('./dist/assets/images'))
+})
+
+// ブラウザ自動更新
+gulp.task('serve', (done) => {
+  browserSync.init(browserSyncOption)
+  done()
+})
+/*===================================================
+          watchタスク
+===================================================*/
+// watch実行
+gulp.task('watch', (done) => {
+  const browserReload = (done) => {
+    browserSync.reload()
+    done()
+  }
+  gulp.watch('./src/assets/sass/**/*.scss', gulp.series('sass'))
+  gulp.watch('./dist/**/*', browserReload)
+})
+
+gulp.task('default', gulp.series('serve', 'watch'))
